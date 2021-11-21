af163665aef0876b5884aa6e1ab22108e64f2eec
Maxence Bonnet <bonnet.maxence80@gmail.com>
2021-11-21 12:43:47 +0100

    Fixed: some quick fixes (end of file new line, shorten templates, attributes triggering invalid files warning)

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -31,18 +31,9 @@ class Comment
     /**
      * @ORM\Column(type="text")
      */
-    #[Assert\NotBlank(
-        message: 'Le message ne peut pas être vide.'
-    )]
-    #[Assert\NotNull(
-        message: 'Le message ne peut pas être vide.'
-    )]
-    #[Assert\Length(
-        min: 2,
-        max: 500,
-        minMessage: 'Le message doit avoir au minimum {{ limit }} caractères.',
-        maxMessage: 'Le message trick doit avoir au maximum {{ limit }} caractères.'
-    )]
+    #[Assert\NotBlank(message: 'Le message ne peut pas être vide.')]
+    #[Assert\NotNull(message: 'Le message ne peut pas être vide.')]
+    #[Assert\Length(min: 2, max: 500, minMessage: 'Le message doit avoir au minimum {{ limit }} caractères.', maxMessage: 'Le message trick doit avoir au maximum {{ limit }} caractères.')]
     private $content;
 
     /**
